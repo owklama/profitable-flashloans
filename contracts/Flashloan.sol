ee2275fd0bdb8131d6132cba5a9007a2e8cdf882
Dean Shaked <deanshak9@gmail.com>
2022-03-20 23:04:56 +0200

    ..

---
+++ contracts/Flashloan.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+import "@studydefi/money-legos/dydx/contracts/DydxFlashloanBase.sol";
+import "@studydefi/money-legos/dydx/contracts/ICallee.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract Flashloan is ICallee, DydxFlashloanBase {
+    enum Direction { KyberToUniswap, UniswapToKyber } 
+    struct ArbInfo {
+        Direction direction;
+        uint repayAmount;
+    }
+
+    // This is the function that will be called postLoan
+    // i.e. Encode the logic to handle your flashloaned funds here
+    function callFunction(
+        address sender,
+        Account.Info memory account,
+        bytes memory data
+    ) public {
+        ArbInfo memory arbInfo = abi.decode(data, (ArbInfo));
+        // Enter arbitage logic here
+    }
+
+    function initiateFlashloan(
+      address _solo, 
+      address _token, 
+      uint256 _amount,
+      Direction _direction) 
+        external 
+    {
+        ISoloMargin solo = ISoloMargin(_solo);
+
+        // Get marketId from token address
+        uint256 marketId = _getMarketIdFromTokenAddress(_solo, _token);
+
+        // Calculate repay amount (_amount + (2 wei))
+        // Approve transfer from
+        uint256 repayAmount = _getRepaymentAmountInternal(_amount);
+        IERC20(_token).approve(_solo, repayAmount);
+
+        // 1. Withdraw $
+        // 2. Call callFunction(...)
+        // 3. Deposit back $
+        Actions.ActionArgs[] memory operations = new Actions.ActionArgs[](3);
+
+        operations[0] = _getWithdrawAction(marketId, _amount);
+        operations[1] = _getCallAction(
+            // Encode MyCustomData for callFunction
+            abi.encode(ArbInfo({direction: _direction, repayAmount: repayAmount}))
+        );
+        operations[2] = _getDepositAction(marketId, repayAmount);
+
+        Account.Info[] memory accountInfos = new Account.Info[](1);
+        accountInfos[0] = _getAccountInfo();
+
+        solo.operate(accountInfos, operations);
+    }
+}
